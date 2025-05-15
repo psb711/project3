815c6f39f5986b65918c191104bcc5558ec1bd87
psb711 <711psb@naver.com>
2025-05-15 19:03:24 +0900

    20250515

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {
   
-  $("#AllCat").click(function () {
-    $(".sub").stop().slideToggle();
-  });
-
+ 
  
   $(".AllCatsub").hover(function (e) {
 
@@ -14,8 +11,55 @@ $(document).ready(function () {
 
     e.stopPropagation();
   });
-
-  $("#BRN h5").click(function () {
-  $(this).next("p").stop().slideToggle();
+// 푸터 사업자 정보 슬라이드토글
+  $(".BizInfo").click(function () {
+   $(this).nextAll("p").first().stop().slideToggle();
 });
+//메인메뉴 
+
+
+ let isOpen = false;
+
+ 
+  $("#AllCat").click(function (e) {
+    e.stopPropagation(); 
+    e.preventDefault();
+    if (isOpen) {
+      $(".subwrap").stop().slideUp();
+      $("#AllCat").removeClass("selected");
+    } else {
+      $(".subwrap").stop().slideDown();
+      $(this).addClass("selected");
+    }
+    isOpen = !isOpen;
+  });
+
+  $(window).click(function(){
+    isOpen = false;
+    $(".subwrap").stop().slideUp();
+    $("#AllCat").removeClass("selected");
+  })
+// 버튼박스 글씨 호버
+$(".circlebutton").hover(
+  function () {
+    const index = $(".circlebutton").index(this);
+    $("#mbuttonbox p").eq(index).css({
+   
+      transition: "transform 0.2s",
+      fontWeight: "600"
+    });
+  },
+  function () {
+    const index = $(".circlebutton").index(this);
+    $("#mbuttonbox p").eq(index).css({
+      
+      transition: "transform 0.2s",
+       fontWeight: "500"
+    });
+  }
+);
+
+    
+ 
+
 });
