9afffdda06e1ab4f6ae0e4b3670c6bcfa77329c4
psb711 <711psb@naver.com>
2025-05-26 23:22:33 +0900

    20250526(2)

--- js/Book.js
+++ js/Book.js
@@ -27,13 +27,13 @@ async function fetchBooks(query) {
 
 async function bookData0() {
    try {
-      const querys = ['빛들의 환대','사람은 무엇으로 사는가','감정수업','탁영','파과','나는 어떻게 삶의 해답을 찾는가','호랑골동품점'];
+      const querys = ['빛들의 환대', '사람은 무엇으로 사는가', '감정수업', '탁영', '파과', '나는 어떻게 삶의 해답을 찾는가', '호랑골동품점'];
 
       querys.forEach(async (query, i) => {
          const data = await fetchBooks(query);
 
          const divs = $('#book1-1 .bookbox')
-   
+
          divs.eq(i).append(`
             <a href="">
                <div class="bookbox1">
@@ -44,31 +44,47 @@ async function bookData0() {
                </div>
             </a>`);
       })
-   
+
    } catch (error) {
       console.log('에러발생', error);
    }
 }
+
 async function bookData1() {
    try {
-      const querys = ['소설','기욤뮈소','박완서'];
-     querys.forEach(async (query, i) => {
+      const querys = ['정치', '경제', '에세이'];
+
+      querys.forEach(async (query, i) => {
          const data = await fetchBooks(query);
 
-         // for문 (10개)
-   const firstBox = $('.book2box1');
+         //썸네일이 빈 문자열인것은 제외
+         const origin = data.documents;
+         let book = origin.filter((val) => {
+            return val.thumbnail != '' && val.contents != '';
+         })
 
-         for (let j = 0; j<6; j++) {
-            firstBox.eq(j).append(`
-               <a href="">
+         // for문 (8개)
+         for (let j = 0; j < 6; j++) {
+
+            $('.book2box').eq(i).append('<div class="book2box1"></div>');
+            const div = $('.book2box').eq(i).find('.book2box1').last();
+              let content = data.documents[j].contents;
+            let str = content.substring(0, 50);
+            div.append(`<a href="">
                   <div class="book2box1-Thumnail">
                      <img src=${data.documents[j].thumbnail}>
                   </div>
                   <div class="titbox2">
                      <h4>${data.documents[j].title}</h4>
-                     <p>${data.documents[j].title}</p>
+                     <p>${data.documents[j].authors }</p>
+                     <p>${str }...</p>
                   </div>
-               </a>`);
+               </a>`)
+          
+
+        
+
+
          }
       })
    } catch (error) {
@@ -76,6 +92,50 @@ async function bookData1() {
    }
 }
 
+async function bookData4() {
+   try {
+      const querys = ['인기'];
+
+      querys.forEach(async (query, i) => {
+         const data = await fetchBooks(query);
+
+         // 썸네일, 내용 없는 책 제외
+         const origin = data.documents;
+         const book = origin.filter((val) => {
+            return val.thumbnail != '' && val.contents != '';
+         });
+
+         // 최대 18권 출력 (최소 book.length 확인)
+         for (let j = 0; j < Math.min(18, book.length); j++) {
+            $('.TLankbox').eq(i).append('<div class="TLankbox1"></div>');
+            const div = $('.TLankbox').eq(i).find('.TLankbox1').last();
+
+            let content = book[j].contents;
+            let str = content.substring(0, 50);
+
+            div.append(`
+               <a href="">
+                  <div class="book2box1-Thumnail">
+                     <img src="${book[j].thumbnail}" alt="책 썸네일">
+                  </div>
+                  <div class="titbox2">
+                     <h4>${book[j].title}</h4>
+                     <p>${book[j].authors}</p>
+                     <p>${str}...</p>
+                  </div>
+               </a>
+            `);
+         }
+      });
+   } catch (error) {
+      console.log('에러 발생:', error);
+   }
+}
+
+
 
 bookData0();
 bookData1();
+bookData2();
+bookData3();
+bookData4();
