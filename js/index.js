eec1bddcd7767aa4d93d5906b1fe8a596c622c9e
psb711 <711psb@naver.com>
2025-05-29 18:09:07 +0900

    20250529(2)

--- js/index.js
+++ js/index.js
@@ -138,7 +138,7 @@ $(function () {
         'pointer-events': 'auto'
       });
     } else {
-      $('.s1 .leftbutton, .rightbutton').css({
+      $('.s1 .leftbutton,.s1 .rightbutton').css({
         opacity: 1,
         'pointer-events': 'auto'
       });
@@ -148,58 +148,14 @@ $(function () {
 
 
 });
+// 배너버튼
+ 
+$(function () {
 
-  // 오늘리디의발견 슬라이드
-
-// $(function () {
-//   const $slider = $('#book1 .arrybox');
-//   const $leftBtn = $('#book1 .leftbutton');
-//   const $rightBtn = $('#book1 .rightbutton');
-//   const slideWidth = 215;
-//   const totalSlides = $slider.find('.bookbox').length;
-//   let currentIndex = 0;
-
-//   updateButtons();
-
-//   $leftBtn.click(function () {
-//     if ($slider.is(':animated') || $leftBtn.css('opacity') == '0') return;
-
-//     currentIndex--;
-//     if (currentIndex < 0) currentIndex = 0;
-
-//     $slider.find('.bookbox:last').prependTo($slider);
-//     $slider.css('margin-left', -slideWidth);
-//     $slider.stop().animate({ marginLeft: 0 }, 700, function () {
-//       updateButtons();
-//     });
-//   });
-
-//   $rightBtn.click(function () {
-//     if ($slider.is(':animated') || $rightBtn.css('opacity') == '0') return;
-
-//     currentIndex++;
-//     if (currentIndex >= totalSlides - 1) currentIndex = totalSlides - 1;
-
-//     $slider.stop().animate({ marginLeft: -slideWidth }, 700, function () {
-//       $slider.find('.bookbox:first').appendTo($slider);
-//       $slider.css({ marginLeft: 0 });
-//       updateButtons();
-//     });
-//   });
-
-//   function updateButtons() {
-//     if (currentIndex <= 0) {
-//       $leftBtn.css({ opacity: 0, pointerEvents: 'none' });
-//       $rightBtn.css({ opacity: 1, pointerEvents: 'auto' });
-//     } else if (currentIndex >= totalSlides - 1) {
-//       $leftBtn.css({ opacity: 1, pointerEvents: 'auto' });
-//       $rightBtn.css({ opacity: 0, pointerEvents: 'none' });
-//     } else {
-//       $leftBtn.css({ opacity: 1, pointerEvents: 'auto' });
-//       $rightBtn.css({ opacity: 1, pointerEvents: 'auto' });
-//     }
-//   }
-// });
+  document.getElementById("closeBtn").addEventListener("click", function () {
+    document.getElementById("banner").style.display = "none";
+  });});
+//  오늘 리디의 발견 슬라이드
 $(function () {
   let page = 0;
   const slideWidth = 256;
@@ -244,47 +200,142 @@ $(function () {
 });
 
   // 한주,분야별 리디랭킹 슬라이드
-
   // 정치
-  $('#book2 .leftbuttone').click(function () {
-    $('#book2 #pol .book2box1:last').prependTo('#book2 #pol');
-    $('#book2 #pol').css('margin-left', -403);
-    $('#book2 #pol').stop().animate({ marginLeft: 0 }, 700);
+$(function () {
+
+  let page = 0;
+  const slideWidth = 403;
+  const $slider = $('#book2 #pol');
+  const $leftBtn = $('#book2 .leftbuttone');
+  const $rightBtn = $('#book2 .rightbuttone');
+  const maxPage = 3;
+
+  updateButtons();
+
+  $leftBtn.click(function () {
+    
+
+    page--;
+    $slider.stop().animate({ marginLeft: -slideWidth * page }, 700);
+    updateButtons();
+    if (page <= 0) return;
   });
 
-  $('#book2 .rightbuttone').click(function () {
-    $('#book2 #pol').stop().animate({ marginLeft: -403 }, 700, function () {
-      $('#book2 #pol .book2box1:first').appendTo('#book2 #pol');
-      $('#book2 #pol').css({ marginLeft: 0 });
-    });
+  $rightBtn.click(function () {
+    
+
+    page++;
+    $slider.stop().animate({ marginLeft: -slideWidth * page }, 700);
+    updateButtons();
+    if (page >= maxPage) return;
+  });
+
+  function updateButtons() {
+    if (page <= 0) {
+      $leftBtn.css({ opacity: 0, pointerEvents: 'none' });
+      $rightBtn.css({ opacity: 1, pointerEvents: 'auto' });
+    } else if (page >= maxPage) {
+      $leftBtn.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn.css({ opacity: 0, pointerEvents: 'none' });
+    } else {
+      $leftBtn.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn.css({ opacity: 1, pointerEvents: 'auto' });
+    }
+
+  }
   });
 
   // 경제
-  $('#book2 .leftbuttonr').click(function () {
-    $('#book2 #econ .book2box1:last').prependTo('#book2 #econ');
-    $('#book2 #econ').css('margin-left', -403);
-    $('#book2 #econ').stop().animate({ marginLeft: 0 }, 700);
+
+  $(function () {
+  let page1 = 0;
+  const slideWidth = 403;
+  const $slider1 = $('#book2 #econ');
+  const $leftBtn1 = $('#book2 .leftbuttonr');
+  const $rightBtn1 = $('#book2 .rightbuttonr');
+  const maxPage1 = 3;
+
+  updateButtons();
+
+  $leftBtn1.click(function () {
+    
+
+    page1--;
+    $slider1.stop().animate({ marginLeft: -slideWidth * page1 }, 700);
+    updateButtons();
+    if (page1 <= 0) return;
   });
 
-  $('#book2 .rightbuttonr').click(function () {
-    $('#book2 #econ').stop().animate({ marginLeft: -403 }, 700, function () {
-      $('#book2 #econ .book2box1:first').appendTo('#book2 #econ');
-      $('#book2 #econ').css({ marginLeft: 0 });
-    });
+  $rightBtn1.click(function () {
+    
+
+    page1++;
+    $slider1.stop().animate({ marginLeft: -slideWidth * page1 }, 700);
+    updateButtons();
+    if (page1 >= maxPage1) return;
   });
+
+  function updateButtons() {
+    if (page1 <= 0) {
+      $leftBtn1.css({ opacity: 0, pointerEvents: 'none' });
+      $rightBtn1.css({ opacity: 1, pointerEvents: 'auto' });
+    } else if (page1 >= maxPage1) {
+      $leftBtn1.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn1.css({ opacity: 0, pointerEvents: 'none' });
+    } else {
+      $leftBtn1.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn1.css({ opacity: 1, pointerEvents: 'auto' });
+    }
+
+  }
+  });
+
+
   // 에세이
-  $('#book2 .leftbuttont').click(function () {
-    $('#book2 #Ess .book2box1:last').prependTo('#book2 #Ess');
-    $('#book2 #Ess').css('margin-left', -403);
-    $('#book2 #Ess').stop().animate({ marginLeft: 0 }, 700);
+
+$(function () {
+  let page2 = 0;
+  const slideWidth = 403;
+  const $slider2 = $('#book2 #Ess');
+  const $leftBtn2 = $('#book2 .leftbuttont');
+  const $rightBtn2 = $('#book2 .rightbuttont');
+  const maxPage2 = 3;
+
+  updateButtons2();
+
+  $leftBtn2.click(function () {
+    
+
+    page2--;
+    $slider2.stop().animate({ marginLeft: -slideWidth * page2 }, 700);
+    updateButtons2();
+    if (page2 <= 0) return;
   });
 
-  $('#book2 .rightbuttont').click(function () {
-    $('#book2 #Ess').stop().animate({ marginLeft: -403 }, 700, function () {
-      $('#book2 #Ess .book2box1:first').appendTo('#book2 #Ess');
-      $('#book2 #Ess').css({ marginLeft: 0 });
-    });
+  $rightBtn2.click(function () {
+    
+
+    page2++;
+    $slider2.stop().animate({ marginLeft: -slideWidth * page2 }, 700);
+    updateButtons2();
+    if (page2 >= maxPage2) return;
   });
+
+  function updateButtons2() {
+    if (page2 <= 0) {
+      $leftBtn2.css({ opacity: 0, pointerEvents: 'none' });
+      $rightBtn2.css({ opacity: 1, pointerEvents: 'auto' });
+    } else if (page2 >= maxPage2) {
+      $leftBtn2.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn2.css({ opacity: 0, pointerEvents: 'none' });
+    } else {
+      $leftBtn2.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn2.css({ opacity: 1, pointerEvents: 'auto' });
+    }
+
+  }
+  });
+
 
   //md'spick
   $('#book3 .leftbutton').click(function () {
@@ -300,181 +351,183 @@ $(function () {
     });
   });
 
+$(function () {
+  let page3 = 0;
+  const slideWidth = 426.662;
+  const $slider3 = $('#book3 .mdbox');
+  const $leftBtn3 = $('#book3 .leftbutton');
+  const $rightBtn3 = $('#book3 .rightbutton');
+  const maxPage3 = 2;
+
+  updateButtons3();
+
+  $leftBtn3.click(function () {
+    
+
+    page3--;
+    $slider3.stop().animate({ marginLeft: -slideWidth * page3 }, 700);
+    updateButtons3();
+    if (page2 <= 0) return;
+  });
+
+  $rightBtn3.click(function () {
+    
+
+    page3++;
+    $slider3.stop().animate({ marginLeft: -slideWidth * page3 }, 700);
+    updateButtons3();
+    if (page3 >= maxPage3) return;
+  });
+
+  function updateButtons3() {
+    if (page3 <= 0) {
+      $leftBtn3.css({ opacity: 0, pointerEvents: 'none' });
+      $rightBtn3.css({ opacity: 1, pointerEvents: 'auto' });
+    } else if (page3 >= maxPage3) {
+      $leftBtn3.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn3.css({ opacity: 0, pointerEvents: 'none' });
+    } else {
+      $leftBtn3.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn3.css({ opacity: 1, pointerEvents: 'auto' });
+    }
+
+  }
+  });
+
   //지금 많이 읽고있는 작품
 $(function () {
-  const $slider5 = $('#TLankDiv .TLankbox');
-  const $leftBtn = $('#TLankDiv .leftbutton');
-  const $rightBtn = $('#TLankDiv .rightbutton');
+  let page4 = 0;
   const slideWidth = 1260;
+  const $slider4 = $('#TLankDiv .TLankbox');
+  const $leftBtn4 = $('#TLankDiv .leftbutton');
+  const $rightBtn4 = $('#TLankDiv .rightbutton');
+  const maxPage4 = 1;
 
-  updateButtons();
+  updateButtons4();
 
-  $leftBtn.click(function () {
-    if ($slider5.is(':animated') || $leftBtn.css('opacity') === '0') return;
+  $leftBtn4.click(function () {
+    
 
-    $slider5.find('.TLankbox2:last').prependTo($slider5);
-    $slider5.css('margin-left', -slideWidth);
-    $slider5.stop().animate({ marginLeft: 0 }, 1200, function () {
-      updateButtons();
-    });
+    page4--;
+    $slider4.stop().animate({ marginLeft: -slideWidth * page4 }, 700);
+    updateButtons4();
+    if (page4 <= 0) return;
   });
 
-  $rightBtn.click(function () {
-    if ($slider5.is(':animated') || $rightBtn.css('opacity') === '0') return;
+  $rightBtn4.click(function () {
+    
 
-    $slider5.stop().animate({ marginLeft: -slideWidth }, 1200, function () {
-      $slider5.find('.TLankbox2:first').appendTo($slider5);
-      $slider5.css({ marginLeft: 0 });
-      updateButtons();
-    });
+    page4++;
+    $slider4.stop().animate({ marginLeft: -slideWidth * page4 }, 700);
+    updateButtons4();
+    if (page4 >= maxPage4) return;
   });
 
-  function updateButtons() {
-    const firstId = $slider5.find('.TLankbox2').first().attr('id');
-    const lastId = $slider5.find('.TLankbox2').last().attr('id');
-
-    // 시작 슬라이드 ID와 마지막 슬라이드 ID를 여기에 맞게 설정하세요
-    if (firstId === 'TLankbox2slide1') {
-      $leftBtn.css({ opacity: 0, pointerEvents: 'none' });
-      $rightBtn.css({ opacity: 1, pointerEvents: 'auto' });
-    } else if (lastId === 'TLankbox2slide2') {
-      $leftBtn.css({ opacity: 1, pointerEvents: 'auto' });
-      $rightBtn.css({ opacity: 0, pointerEvents: 'none' });
+  function updateButtons4() {
+    if (page4 <= 0) {
+      $leftBtn4.css({ opacity: 0, pointerEvents: 'none' });
+      $rightBtn4.css({ opacity: 1, pointerEvents: 'auto' });
+    } else if (page4 >= maxPage4) {
+      $leftBtn4.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn4.css({ opacity: 0, pointerEvents: 'none' });
     } else {
-      $leftBtn.css({ opacity: 1, pointerEvents: 'auto' });
-      $rightBtn.css({ opacity: 1, pointerEvents: 'auto' });
+      $leftBtn4.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn4.css({ opacity: 1, pointerEvents: 'auto' });
     }
+
   }
-});
+  });
 
-
-
-
-  // $('#TLankDiv .leftbutton').click(function () {
-  //   $('#TLankDiv .TLankbox2:last').prependTo('#TLankDiv .TLankbox');
-  //   $('#TLankDiv .TLankbox').css('margin-left', -1260);
-  //   $('#TLankDiv .TLankbox').stop().animate({ marginLeft: 0 }, 1200);
-
-  // });
-
-  // $('#TLankDiv .rightbutton').click(function () {
-  //   $('#TLankDiv .TLankbox').stop().animate({ marginLeft: -1260 }, 1200, function () {
-  //     $('#TLankDiv .TLankbox2:first').appendTo('#TLankDiv .TLankbox');
-  //     $('#TLankDiv .TLankbox').css({ marginLeft: 0 });
-  //   });
-  // });
 
 // 지금리디에서만? 선출간 신작
 
-$('#book4 .leftbutton').click(function () {
-  const $slider = $('#book4 .arrybox');
-  if ($slider.is(':animated')) return;
+$(function () {
+  let page5 = 0;
+  const slideWidth5 = 256;
+  const $slider5 = $('#book4 .arrybox');
+  const $leftBtn5 = $('#book4 .leftbutton');
+  const $rightBtn5 = $('#book4 .rightbutton');
+  const maxPage5 = 5;
 
-  $slider.find('.bookbox:last').prependTo($slider);
-  $slider.css('margin-left', -215);
-  $slider.stop().animate({ marginLeft: 0 }, 700);
-});
+  updateButtons5();
 
-$('#book4 .rightbutton').click(function () {
-  const $slider = $('#book4 .arrybox');
-  if ($slider.is(':animated')) return;
+  $leftBtn5.click(function () {
+    
 
-  $slider.stop().animate({ marginLeft: -215 }, 700, function () {
-    $slider.find('.bookbox:first').appendTo($slider);
-    $slider.css({ marginLeft: 0 });
+    page5--;
+    $slider5.stop().animate({ marginLeft: -slideWidth5 * page5 }, 700);
+    updateButtons5();
+    if (page5 <= 0) return;
   });
-});
+
+  $rightBtn5.click(function () {
+    
+
+    page5++;
+    $slider5.stop().animate({ marginLeft: -slideWidth5 * page5 }, 700);
+    updateButtons5();
+    if (page5 >= maxPage5) return;
+  });
+
+  function updateButtons5() {
+    if (page5 <= 0) {
+      $leftBtn5.css({ opacity: 0, pointerEvents: 'none' });
+      $rightBtn5.css({ opacity: 1, pointerEvents: 'auto' });
+    } else if (page5 >= maxPage5) {
+      $leftBtn5.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn5.css({ opacity: 0, pointerEvents: 'none' });
+    } else {
+      $leftBtn5.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn5.css({ opacity: 1, pointerEvents: 'auto' });
+    }
+
+  }
+  });
 
 // 새로나온작품
 
+$(function () {
+  let page6 = 0;
+  const slideWidth6 = 256;
+  const $slider6 = $('#book5 .arrybox');
+  const $leftBtn6 = $('#book5 .leftbutton');
+  const $rightBtn6 = $('#book5 .rightbutton');
+  const maxPage6 = 5;
 
-$
-  const $slider = $('#book5 .arrybox');
-  const totalItems = $slider.find('.bookbox').length;
-  const visibleCount = 4;
-  let currentIndex = 0;
+  updateButtons6();
 
-  // 초기 상태: 왼쪽 버튼 투명하고 클릭 불가, 오른쪽 버튼 보이고 클릭 가능
-  $('#book5 .leftbutton').css({
-    opacity: 0,
-    'pointer-events': 'none'
-  });
-  $('#book5 .rightbutton').css({
-    opacity: 1,
-    'pointer-events': 'auto'
-  });
+  $leftBtn6.click(function () {
+    
 
-  $('#book5 .leftbutton').click(function () {
-    if ($slider.is(':animated')) return;
-
-    $slider.find('.ebox1:last').prependTo($slider);
-    $slider.css('margin-left', -368.66);
-    $slider.stop().animate({ marginLeft: 0 }, 700, function() {
-      currentIndex--;
-      updateButtons();
-    });
+    page6--;
+    $slider6.stop().animate({ marginLeft: -slideWidth6 * page6 }, 700);
+    updateButtons6();
+    if (page6 <= 0) return;
   });
 
-  $('#book5 .rightbutton').click(function () {
-    if ($slider.is(':animated')) return;
+  $rightBtn6.click(function () {
+    
 
-    $slider.stop().animate({ marginLeft: -368.66 }, 700, function () {
-      $slider.find('.bookbox:first').appendTo($slider);
-      $slider.css({ marginLeft: 0 });
-      currentIndex++;
-      updateButtons();
-    });
+    page6++;
+    $slider6.stop().animate({ marginLeft: -slideWidth6 * page6 }, 700);
+    updateButtons6();
+    if (page6 >= maxPage6) return;
   });
 
-  function updateButtons() {
-    if (currentIndex <= 0) {
-      currentIndex = 0;
-      $('#book5 .leftbutton').css({
-        opacity: 0,
-        'pointer-events': 'none'
-      });
-      $('#book5 .rightbutton').css({
-        opacity: 1,
-        'pointer-events': 'auto'
-      });
-    } else if (currentIndex >= totalItems - visibleCount) {
-      currentIndex = totalItems - visibleCount;
-      $('#book5 .rightbutton').css({
-        opacity: 0,
-        'pointer-events': 'none'
-      });
-      $('#book5 .leftbutton').css({
-        opacity: 1,
-        'pointer-events': 'auto'
-      });
+  function updateButtons6() {
+    if (page6 <= 0) {
+      $leftBtn6.css({ opacity: 0, pointerEvents: 'none' });
+      $rightBtn6.css({ opacity: 1, pointerEvents: 'auto' });
+    } else if (page6 >= maxPage6) {
+      $leftBtn6.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn6.css({ opacity: 0, pointerEvents: 'none' });
     } else {
-      $('#book5 .leftbutton, #book5 .rightbutton').css({
-        opacity: 1,
-        'pointer-events': 'auto'
-      });
+      $leftBtn6.css({ opacity: 1, pointerEvents: 'auto' });
+      $rightBtn6.css({ opacity: 1, pointerEvents: 'auto' });
     }
+
   }
-
-
-// $('#book5 .leftbutton').click(function () {
-//   const $slider = $('#book5 .arrybox');
-//   if ($slider.is(':animated')) return;
-
-//   $slider.find('.bookbox:last').prependTo($slider);
-//   $slider.css('margin-left', -215);
-//   $slider.stop().animate({ marginLeft: 0 }, 700);
-// });
-
-// $('#book5 .rightbutton').click(function () {
-//   const $slider = $('#book5 .arrybox');
-//   if ($slider.is(':animated')) return;
-
-//   $slider.stop().animate({ marginLeft: -215 }, 700, function () {
-//     $slider.find('.bookbox:first').appendTo($slider);
-//     $slider.css({ marginLeft: 0 });
-//   });
-// });
-
+  });
 // 한주 한책, 셀렉트 레터 +
 $(function() {
   const $slider = $('#book6 .ebox');
